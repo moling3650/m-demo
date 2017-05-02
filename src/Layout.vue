398912a6a52f2ef09bc9c25e0a14c3151666ff2c
moling <365024424@qq.com>
2017-05-02 15:07:41 +0800

    定义布局主组件

---
+++ src/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout">
+    <header class="app-header">header</header>
+    <main class="app-content">content</main>
+    <footer class="app-footer">footer</footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'layout'
+  }
+</script>
+
+<style scoped>
+</style>
